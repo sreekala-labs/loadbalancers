77d7e931f73b5d0b3964adf426126af1d8a731b2
Sreenath Kalahasti <sreenath.kalahasti@gmail.com>
2026-03-09 20:10:08 -0700

    Fix print statement formatting in logic.py
    Add a newline at the end of the print statement.

---
+++ weightedroundrobin/logic.py
@@ -0,0 +1,26 @@
+#Iteration tools which is used for fast and efficient iteration and baiscyally recycles itself to the beginning and also used when there is infinite numbers.
+import itertools
+class WeightedRoundRobin:
+  #Constructor
+  def __init__(self, servers):
+    #Declare an array of weighted pool.
+    self.weighted_pool=[]
+    for server, weight in servers.items():
+      self.weighted_pool.extend([server] * weight)
+
+    self.selector= itertools.cycle(self.weighted_pool)
+
+  def get_next_server(self):
+    return next(self.selector)
+    
+#Dictionary of server and weight.
+servers={
+  "Server A":3,
+  "Server B":2,
+  "Server C":1
+}
+
+lb=WeightedRoundRobin(servers)
+for i in range(1,13):
+  current_server= lb.get_next_server()
+  print(f"Request : {0:2d} --> {current_server}")
